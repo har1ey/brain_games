893992621f9d4099794ece2101d1635d5f0df377
Nikolai Vakhrushev <nik.vakhrushev@gmail.com>
2017-06-27 22:15:13 +0700

    add welcome() and index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import welcome from '..';
 
 console.log('Welcome to the Brain Games!');
+
+welcome();
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const welcome = () => {
+
+    const actual = readlineSync.question('May I have your name? ');
+
+    console.log(`Hello, ${actual}!`);
+};
+
+export default welcome;
