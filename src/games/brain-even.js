d98134d34111412c400c71b8af69b1b13a9cc8a4
Nikolai Vakhrushev <nik.vakhrushev@gmail.com>
2017-06-28 17:48:06 +0700

    fix 'if' constructions

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -7,28 +7,28 @@ console.log('Answer "yes" if number even otherwise answer "no"\n');
 
 const actual = welcome();
 
+const checkNumber = (number) => {
+  if (number % 2 === 0) return 'yes';
 
-const even = () => {
+  return 'no';
+};
+
+const thisGame = () => {
   let count = 0;
 
   while (count !== 3) {
     const number = Math.floor(Math.random() * 100);
     console.log(`Question: ${number}`);
+    const checkAnswer = checkNumber(number);
+
     const answer = readlineSync.question('Your answer: ');
 
-
-    if ((answer !== 'yes') && (answer !== 'no')) {
-      console.log('Please correct input: \'yes\' or \'no\'');
-      break;
-    } else if (((answer === 'yes') === (number % 2 === 0)) || ((answer === 'no') === (number % 2 !== 0))) {
-      console.log('Correct!');
-      count += 1;
-    } else if (answer === 'yes') {
-      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+    if (answer !== checkAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkAnswer}'.`);
       break;
     } else {
-      console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
-      break;
+      console.log('Correct!');
+      count += 1;
     }
   }
 
@@ -36,4 +36,4 @@ const even = () => {
   else console.log(`Let's try again, ${actual}!`);
 };
 
-export default even;
+export default thisGame;
