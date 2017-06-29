184c19fa16039ca1134ce45c2186cd61c6cca01e
Nikolai Vakhrushev <nik.vakhrushev@gmail.com>
2017-06-29 11:54:22 +0700

    add hexlet-pair, refactoring code

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import welcome from '..';
+//  import readlineSync from 'readline-sync';
+//  import { cons, car, cdr, toString } from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no"\n');
+import startGames from '..';
 
-const actual = welcome();
+const description = 'Answer "yes" if number even otherwise answer "no"\n';
 
 const checkNumber = (number) => {
   if (number % 2 === 0) return 'yes';
@@ -13,27 +13,9 @@ const checkNumber = (number) => {
   return 'no';
 };
 
-const thisGame = () => {
-  let count = 0;
+const genRandom = () => Math.floor(Math.random() * 100);
 
-  while (count !== 3) {
-    const number = Math.floor(Math.random() * 100);
-    console.log(`Question: ${number}`);
-    const checkAnswer = checkNumber(number);
-
-    const answer = readlineSync.question('Your answer: ');
-
-    if (answer !== checkAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkAnswer}'.`);
-      break;
-    } else {
-      console.log('Correct!');
-      count += 1;
-    }
-  }
-
-  if (count === 3) console.log(`Congratulations, ${actual}!`);
-  else console.log(`Let's try again, ${actual}!`);
-};
+const thisGame = () => startGames(cons(description, cons(genRandom, checkNumber)));
+//  const checkAnswer = checkNumber(number);
 
 export default thisGame;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import readlineSync from 'readline-sync';
+// import { cons, car, cdr, toString } from 'hexlet-pairs';
+import { car, cdr } from 'hexlet-pairs';
 
 
-const welcome = () => {
+const startGames = (gameOptions) => {
+  const data = gameOptions;
+  const description = car(data);
+
+  console.log('Welcome to the Brain Games!');
+  console.log(`${description}\n`);
+
   const actual = readlineSync.question('May I have your name? ');
-
   console.log(`Hello, ${actual}!\n`);
 
-  return actual;
+  let count = 0;
+
+  while (count !== 3) {
+    const randomNumber = car(cdr(data));
+    const number = randomNumber();
+
+    console.log(`Question: ${number}`);
+    const checkNumber = cdr(cdr(data));
+    const checkAnswer = checkNumber(number);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer !== checkAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkAnswer}'.`);
+      break;
+    } else {
+      console.log('Correct!');
+      count += 1;
+    }
+  }
+
+  if (count === 3) console.log(`Congratulations, ${actual}!`);
+  else console.log(`Let's try again, ${actual}!`);
 };
 
-export default welcome;
+export default startGames;
